6d7d9b14d0a9e519205eb2d73fdbc7e8d057a8b9
courtney-spivey <cvs393@utulsa.edu>
2020-12-14 21:28:29 +0000

    broken art gallery

---
+++ components/Gallery.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="gallery">
+    <div class="gallery-panel"
+         v-for="photo in photos"
+         :key="photo.id">
+      <img :src="thumbUrl(photo.filename)">
+    </div>
+  </div>
+</template>
+
+<script>
+import photos from 'photos.js'; 
+export default {
+  name: 'Gallery',
+  data() {
+    return {
+      photos,
+    };
+  },
+  methods: {
+    thumbUrl(filename) {
+      return require(`../assets/gallery/${filename}`);
+    },
+  },
+};
+</script>
